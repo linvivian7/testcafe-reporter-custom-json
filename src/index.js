476eff5f9629613e7a022892a117385082047b5a
Vivian Lin <linvivian7@gmail.com>
2018-10-29 12:14:55 +0900

    don't include error stack

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-export default function () {
-    return {
-        noColors:       true,
-        currentFixture: null,
+export default function() {
+  return {
+    noColors: true,
+    currentFixture: null,
 
-        report: {
-            passed:   0,
-            total:    0,
-            failedFixtures: []
-        },
+    report: {
+      passed: 0,
+      total: 0,
+      failedFixtures: []
+    },
 
-        reportTaskStart (startTime, userAgents, testCount) {
-            this.report.total      = testCount;
-        },
+    reportTaskStart(startTime, userAgents, testCount) {
+      this.report.total = testCount;
+    },
 
-        reportFixtureStart (name, path) {
-            this.currentFixture = { name, path, tests: [] };
-            this.report.failedFixtures.push(this.currentFixture);
+    reportFixtureStart(name, path) {
+      this.currentFixture = { name, path, tests: [] };
+      this.report.failedFixtures.push(this.currentFixture);
+    },
 
-        },
+    reportTestDone(name, testRunInfo) {
+      var errs = testRunInfo.errs.map(err => this.formatError(err));
 
-        reportTestDone (name, testRunInfo) {
-            var errs = testRunInfo.errs.map(err => this.formatError(err));
+      if (errs.length > 0) {
+        this.currentFixture.tests.push({
+          name
+        });
+      }
+    },
 
-            if (errs.length > 0) {
-                this.currentFixture.tests.push({
-                    name,
-                    errs
-                });
-            }
-        },
+    reportTaskDone(endTime, passed) {
+      this.report.passed = passed;
+      this.report.failedFixtures = this.report.failedFixtures.filter(
+        ({ tests }) => tests.length > 0
+      );
 
-        reportTaskDone (endTime, passed) {
-            this.report.passed   = passed;
-            this.report.failedFixtures = this.report.failedFixtures.filter(({ tests }) => tests.length > 0);
-
-            this.write(JSON.stringify(this.report, null, 2));
-        }
-    };
+      this.write(JSON.stringify(this.report, null, 2));
+    }
+  };
 }
